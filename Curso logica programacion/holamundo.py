5180985d1b5c7d48214301e95b49f0073d666afb
Maguey2022 <116772608+Maguey2022@users.noreply.github.com>
2022-10-28 17:37:26 -0500

    1er subida de archivi de Python

---
+++ Curso logica programacion/holamundo.py
@@ -0,0 +1,11 @@
+#EJEMPLO 1. Saludo
+#COMENTARIOS: Sirven para documentar o detallar cada parte del programa.
+#Los comentarios los omite o no los toma en cuenta Python
+#imprimiendo o mostrando en pantalla mensajes o resultados
+
+
+print("holamundo")
+print('jared')
+print(10+5)
+print(9*8) 
+
