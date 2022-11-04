fbacf951d7370969fe051a2a584cb49a7aa9eca9
Maguey2022 <116772608+Maguey2022@users.noreply.github.com>
2022-11-03 22:52:44 -0600

    Add files via upload

---
+++ Curso logica programacion/operacionesmatematicas.py
@@ -0,0 +1,38 @@
+#Ejemplo 2. Operaciones matematicas
+ 
+
+ #Entradas de datos
+
+
+
+número_1 = 10
+número_2 = 2
+ #Declarar o crear constantes: Elemento que su valor permanece o se mantiene fijo
+PI = 3.1416
+
+
+
+
+ #Procesos (calculos y operaciones matemáticas y/o lógicas: elif y else)
+número_1 + número_2
+número_1 - número_2
+
+número_1 * número_2 
+número_1 / número_2
+
+
+
+
+
+ #Salidas de datos (Mostrar resultados)
+print(número_1 + número_2)
+print("la suma es =", suma)
+print("la suma es = " + str(suma)) #CONCATENACIÓN: Unión de dos o más textos
+# Conversion de un tipo de dato a otro tipo de dato (CASTEO)
+
+print(f"la suma es = {suma}") #Interpolación de textos formateo
+
+print(resta)
+print(número_1+número_2)
+print(número_1/número_2)
+
